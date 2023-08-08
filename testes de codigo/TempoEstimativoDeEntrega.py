762302716075aa1390f5804d64b23e05f878606e
beatriz arantes <beatrizarantes.dev@gmail.com>
2023-08-08 18:39:21 -0300

    Desafio de Código

---
+++ testes de codigo/TempoEstimativoDeEntrega.py
@@ -0,0 +1,6 @@
+# Informar ao usuário o nome do restaurante e o tempo em minutos estimado de entrega
+
+nomeRestaurante = input("=====Restaurante: ")
+tempoEstimadoEntrega = int(input("=====Tempo: "))
+
+print(f"O restaurante {nomeRestaurante} entrega em {tempoEstimadoEntrega} minutos.")
